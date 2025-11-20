385c2089a84c0519b448f851059e36550ff8c29e
Faishal Ahmad Rafi <farmoron@gmail.com>
2025-11-20 10:45:37 +0700

    Player character upda
    Character are now movable and spawn bullet with sound effect but still unable to shot projectile

--- Assets/Script/Karakter.cs
+++ Assets/Script/Karakter.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class Karakter : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Vector2 mousePosition;
+    public GameObject BulletCircle;
+    public Transform ShotNavigation;
+    private float Timer = 0f;
+    private int TotalShot;
+    public int MaximumShot = 1;
+    public AudioSource AudioShot;
     void Start()
     {
         
@@ -11,6 +18,42 @@ public class Karakter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     Debug.Log("lompat");
+        //     jump();
+        // }
+        Timer += Time.deltaTime;
+        if (Timer >= 1f)
+        {
+            Timer = 0f;
+            TotalShot = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (TotalShot < MaximumShot)
+            {
+                TembakPeluru();
+                TotalShot++;
+            }
+        }
+
+        move();
+    }
+    public void move()
+    {
+        Vector2 worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //untuk mengatur posisi
+        mousePosition = new Vector2(worldMousePosition.x, transform.position.y); //kalau sesuai posisi pakai transform.position
+        Vector2 minimum = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        Vector2 maximum = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        mousePosition.x = Mathf.Clamp(mousePosition.x, minimum.x, maximum.x);
+        mousePosition.y = Mathf.Clamp(mousePosition.y, minimum.y, maximum.y);
+        transform.position = mousePosition;
+    }
+    void TembakPeluru()
+    {
+        Instantiate(BulletCircle, ShotNavigation.position, ShotNavigation.rotation);
+        AudioShot.Play();
     }
 }
